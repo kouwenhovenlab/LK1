2995c627820c90833db3af38b8ab1fcc6043e6e1
Local Administrator <LocalAdmin@TUD278005.tudelft.net>
2018-09-28 15:47:50 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LK1',
+    version='0.0.1',
+    description='Measurement code on the LK1 setup',
+    author='Wolfgang Pfaff',
+    author_email='wolfgangpfff@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/kouwenhovenlab',
+)
